309131038657486daa20529682d5e159c2a3d7ef
jacace <javierandrescaceres@gmail.com>
2020-08-24 18:49:28 +0100

    Added iterate function sample

--- app.js
+++ app.js
@@ -6,77 +6,83 @@ const config = require("./config");
 const authenticator = new Gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${config.database}/colls/${config.collection}`, config.primaryKey)
 
 const client = new Gremlin.driver.Client(
-    config.endpoint, 
-    { 
+    config.endpoint,
+    {
         authenticator,
-        traversalsource : "g",
-        rejectUnauthorized : true,
-        mimeType : "application/vnd.gremlin-v2.0+json"
+        traversalsource: "g",
+        rejectUnauthorized: true,
+        mimeType: "application/vnd.gremlin-v2.0+json"
     }
 );
 
 
-function dropGraph()
-{
+function dropGraph() {
     console.log('Running Drop');
-    return client.submit('g.V().drop()', { }).then(function (result) {
+    return client.submit('g.V().drop()', {}).then(function (result) {
         console.log("Result: %s\n", JSON.stringify(result));
     });
 }
 
-function addVertex1()
-{
-    console.log('Running Add Vertex1'); 
+function addVertex1() {
+    console.log('Running Add Vertex1');
     return client.submit("g.addV(label).property('id', id).property('firstName', firstName).property('age', age).property('userid', userid).property('pk', 'pk')", {
-            label:"person",
-            id:"thomas",
-            firstName:"Thomas",
-            age:44, userid: 1
-        }).then(function (result) {
-                console.log("Result: %s\n", JSON.stringify(result));
-        });
-    }
-
-function addVertex2()
-{
-    console.log('Running Add Vertex2');
-    return client.submit("g.addV(label).property('id', id).property('firstName', firstName).property('lastName', lastName).property('age', age).property('userid', userid).property('pk', 'pk')", { 
-            label:"person",
-            id:"mary",
-            firstName:"Mary",
-            lastName: "Andersen",
-            age:39,
-            userid: 2
-        }).then(function (result) {
-            console.log("Result: %s\n", JSON.stringify(result));
-        });
-}
-
-function addEdge()
-{
-    console.log('Running Add Edge'); 
-    return client.submit("g.V(source).addE(relationship).to(g.V(target))", {
-            source:"thomas", 
-            relationship:"knows", 
-            target:"mary"
-        }).then(function (result) {
-            console.log("Result: %s\n", JSON.stringify(result));
-        });
-}
-
-function countVertices()
-{
-    console.log('Running Count');
-    return client.submit("g.V().count()", { }).then(function (result) {
+        label: "person",
+        id: "javier",
+        firstName: "javier",
+        age: 35, userid: 1
+    }).then(function (result) {
         console.log("Result: %s\n", JSON.stringify(result));
     });
 }
 
-function finish()
-{
+function addVertex2() {
+    console.log('Running Add Vertex2');
+    return client.submit("g.addV(label).property('id', id).property('firstName', firstName).property('lastName', lastName).property('age', age).property('userid', userid).property('pk', 'pk')", {
+        label: "person",
+        id: "mary",
+        firstName: "Mary",
+        lastName: "Andersen",
+        age: 39,
+        userid: 2
+    }).then(function (result) {
+        console.log("Result: %s\n", JSON.stringify(result));
+    });
+}
+
+function addEdge() {
+    console.log('Running Add Edge');
+    return client.submit("g.V(source).addE(relationship).to(g.V(target))", {
+        source: "javier",
+        relationship: "knows",
+        target: "mary"
+    }).then(function (result) {
+        console.log("Result: %s\n", JSON.stringify(result));
+    });
+}
+
+function countVertices() {
+    console.log('Running Count');
+    return client.submit("g.V().count()", {}).then(function (result) {
+        console.log("Result: %s\n", JSON.stringify(result));
+    });
+}
+
+function iterateSample() {
+    console.log('Running query example');
+
+    return client.submit("g.V().hasLabel(label).tail(n)", { label: 'person', n: 3 }).then(function (result) {
+        // ResultSet is an iterable
+        for (const vertex of result) {
+            console.log(vertex.id);
+        }
+    });
+
+}
+
+function finish() {
     console.log("Finished");
     console.log('Press any key to exit');
-    
+
     process.stdin.resume();
     process.stdin.on('data', process.exit.bind(process, 0));
 }
@@ -87,15 +93,16 @@ client.open()
     .then(addVertex2)
     .then(addEdge)
     .then(countVertices)
+    .then(iterateSample)
     .catch((err) => {
         console.error("Error running query...");
         console.error(err)
     }).then((res) => {
         client.close();
         finish();
-    }).catch((err) => 
+    }).catch((err) =>
         console.error("Fatal error:", err)
     );
-    
-    
+
+
 
